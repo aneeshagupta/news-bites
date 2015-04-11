5e9cf2f51a39f231e185a379428603ddee5734ca
Aneesha Gupta <aneeshag@usc.edu>
2015-04-11 15:26:26 -0700

    Add data to divs, pull abstracts

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ var result = {
 function svc_search_v2_articlesearch(jsonObj) { 
 		var num = jsonObj.response.docs.length
 		for(i = 0; i<num; i++ ) {
-			document.getElementById('search-results').innerHTML += ' <a href = '+ jsonObj.response.docs[i].web_url+'>'+jsonObj.response.docs[i].headline.print_headline+ '</a> </br> '; 
+			//document.getElementById('search-results').innerHTML += ' <a href = '+ jsonObj.response.docs[i].web_url+'>'+jsonObj.response.docs[i].headline.print_headline+ '</a> </br> '; 
 			var result = new Object(); 
 			result.name = jsonObj.response.docs[i].headline.main; 
 			result.link = jsonObj.response.docs[i].web_url; 
@@ -17,12 +17,22 @@ function svc_search_v2_articlesearch(jsonObj) {
 				result.abstract = jsonObj.response.docs[i].snippet; 
 			}
 			search_results.push(result); 
-			var iDiv = document.createElement('div'); 
+			var iDiv = document.createElement('div');  //creates div for each search result 
 			iDiv.id = 'result'+i; 
-			document.getElementsByTagName('body')[0].appendChild(iDiv); 
+			document.getElementsByTagName('body')[0].appendChild(iDiv); //appends div to HTML 
 		}
 		if(search_results.length == 0) { 
 			alert("No results fit that search criteria.  Please search for something else"); 
+		}
+
+		for(i=0; i<num; i++) { 
+			var item = "result"+i; 
+			$('#'+item).data(search_results[i]); 
+			$('#'+item).html(search_results[i].name); 
+			$('#'+item).click(function(){
+				console.log($(this).data("abstract")); //prints out abstracts for each individual div 
+				
+			});
 		}
 }
 
@@ -33,15 +43,12 @@ $(function() {
 		$('#search-results').html(''); 
 		var searchTerm = $('#search-term').val(); 
 		searchTerm = searchTerm.replace(/\s/g, '+'); 
-		var searchSrc = 'http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?callback=svc_search_v2_articlesearch&q='+searchTerm+'&begin_date=20150101&end_date=20150408&api-key=1005f771230760e52cd130064324c61d%3A19%3A71800087';
+		var searchSrc = 'http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?callback=svc_search_v2_articlesearch&q='+searchTerm+'&begin_date=20150101&end_date=20150411&api-key=1005f771230760e52cd130064324c61d%3A19%3A71800087';
 		var searchScript = document.createElement('script'); 
 		searchScript.src = searchSrc; 
 		$('head').append(searchScript); 
 	}); 
 }); 
-
-
-
 
 
 
