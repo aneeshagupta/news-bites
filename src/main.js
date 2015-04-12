d8290c932077fbb230c67e2b9af29219656be794
Aneesha Gupta <aneeshag@usc.edu>
2015-04-11 17:10:10 -0700

    css borders

--- src/main.js
+++ src/main.js
@@ -7,8 +7,7 @@ var result = {
 
 function svc_search_v2_articlesearch(jsonObj) { 
 		var num = jsonObj.response.docs.length
-		for(i = 0; i<num; i++ ) {
-			//document.getElementById('search-results').innerHTML += ' <a href = '+ jsonObj.response.docs[i].web_url+'>'+jsonObj.response.docs[i].headline.print_headline+ '</a> </br> '; 
+		for(i = 0; i<num; i++ ) { 
 			var result = new Object(); 
 			result.name = jsonObj.response.docs[i].headline.main; 
 			result.link = jsonObj.response.docs[i].web_url; 
@@ -27,6 +26,7 @@ function svc_search_v2_articlesearch(jsonObj) {
 
 		for(i=0; i<num; i++) { 
 			var item = "result"+i; 
+			$('#'+item).addClass("headlines"); 
 			$('#'+item).data(search_results[i]); 
 			$('#'+item).html(search_results[i].name); 
 			$('#'+item).click(function(){
@@ -36,11 +36,10 @@ function svc_search_v2_articlesearch(jsonObj) {
 		}
 }
 
-
 $(function() { 	
 	$('#search-btn').click( function(){
 		search_results = []; 
-		$('#search-results').html(''); 
+		//$('#search-results').html(''); 
 		var searchTerm = $('#search-term').val(); 
 		searchTerm = searchTerm.replace(/\s/g, '+'); 
 		var searchSrc = 'http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?callback=svc_search_v2_articlesearch&q='+searchTerm+'&begin_date=20150101&end_date=20150411&api-key=1005f771230760e52cd130064324c61d%3A19%3A71800087';
