cbbc8cc16aeb64a003f9fffdbf504fe90ffee619
Aneesha Gupta <aneeshag@usc.edu>
2015-04-17 15:03:42 -0700

    Add new headlines and links

--- src/main.js
+++ src/main.js
@@ -37,6 +37,7 @@ function svc_mostpopular_v2_mostviewed(jsonObj) {
 		var result = new Object(); 
 		result.name = jsonObj.results[i].title;
 		result.abstract = jsonObj.results[i].abstract; 
+		result.link = jsonObj.results[i].url; 
 		search_results.push(result); 
 
 		var iDiv = document.createElement('div');  //creates div for each search result 
@@ -64,6 +65,7 @@ $(function() {
 		search_results = []; 
 		$("#results").empty(); 
 		var searchTerm = $('#search-term').val(); 
+	
 		searchTerm = searchTerm.replace(/\s/g, '+'); 
 		var searchSrc = 'http://api.nytimes.com/svc/search/v2/articlesearch.jsonp?callback=svc_search_v2_articlesearch&q='+searchTerm+'&begin_date=20150101&end_date=20150412&api-key=1005f771230760e52cd130064324c61d%3A19%3A71800087';
 		var searchScript = document.createElement('script'); 
@@ -82,6 +84,21 @@ $(function(){
 	$('#international').click(function(){
 		change_headline('international'); 
 	}); 
+	$('#tech').click(function(){
+		change_headline('technology'); 
+	}); 
+	$('#health').click(function(){
+		change_headline('health'); 
+	});
+	$('#science').click(function(){
+		change_headline('science'); 
+	});  
+	$('#travel').click(function(){
+		change_headline('travel'); 
+	}); 
+	$('#business').click(function(){
+		change_headline('business'); 
+	}); 
 
 }); 
 
@@ -92,7 +109,6 @@ function change_headline(keyword) {
 	var script = document.createElement('script'); 
 	script.src = source; 
 	$('head').append(script); 
-
 }
 
 function load_links() { 
@@ -106,7 +122,7 @@ function load_links() {
 
 		$('#'+item).append($('#'+description)); 
 		$('#'+description).addClass("abstracts"); 
-		$('#'+description).html(search_results[i].abstract); 
+		$('#'+description).html(search_results[i].abstract+' </br> <a href = " '+search_results[i].link+' "> Read More </a> ' ); 
 		$('#'+description).hide(); 
 
 		$('#'+item).click(function(){
